a2495b9d55233f43444b361b69985a758ea32674
Greensky <david.hesliere@gmail.com>
2024-09-30 20:43:18 +0200

    chore: 🦺 Ajout d'assertions dans le module rocher

--- modules/rocher.py
+++ modules/rocher.py
@@ -1,6 +1,7 @@
 from p5 import *
 
 from utils.assertions import safe_fill
+from utils.utils import assert_color
 
 
 def rock(x, y, couleur_rocher = (180, 180, 180), couleur_ombre = (150, 150, 150)):
@@ -12,7 +13,12 @@ def rock(x, y, couleur_rocher = (180, 180, 180), couleur_ombre = (150, 150, 150)
     :param couleur_rocher Couleur du rocher
     :param couleur_ombre Couleur de l'ombre
     """
+    # Assertions
+    assert isinstance(x, int) and isinstance(y, int), "Les coordonnées doivent être des entiers"
+    assert assert_color(couleur_rocher), "La couleur du rocher doit être un tuple de 3 entiers"
+    assert assert_color(couleur_ombre), "La couleur de l'ombre doit être un tuple de 3 entiers"
 
+    # Dessin
     draw_rock(x, y, couleur_rocher)
     draw_shadow(x, y, couleur_ombre)
 
@@ -24,6 +30,11 @@ def draw_rock(x, y, couleur = (180, 180, 180)):
     :param y Coordonnée y du centre du rocher
     :param couleur Couleur du rocher
     """
+    # Assertions
+    assert isinstance(x, int) and isinstance(y, int), "Les coordonnées doivent être des entiers"
+    assert assert_color(couleur), "La couleur du rocher doit être un tuple de 3 entiers"
+
+    # Dessin
     safe_fill(couleur)          # Couleur gris pour le rocher
     stroke(100)                  # Couleur des contours (gris foncé)
     strokeWeight(2)              # Épaisseur des contours
@@ -45,6 +56,11 @@ def draw_shadow(x, y, couleur = (150, 150, 150)):
     :param y Coordonnée y du centre du rocher
     :param couleur Couleur de l'ombre
     """
+    # Assertions
+    assert isinstance(x, int) and isinstance(y, int), "Les coordonnées doivent être des entiers"
+    assert assert_color(couleur), "La couleur de l'ombre doit être un tuple de 3 entiers"
+
+    # Dessin
     safe_fill(couleur)          # Couleur plus sombre pour l'ombre
 
     beginShape()
